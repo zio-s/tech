d68d63ee8b3f8159689a3ee6878b9ee7d2bd1415
seming2 <popqr3@naver.com>
2025-06-09 22:10:00 +0900

    tailwindcss 플러그인 추가 설치 react query 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['media2.dev.to', 'cdn.hashnode.com'],
+  },
 };
 
 export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
