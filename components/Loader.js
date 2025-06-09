323c3edb2132eb318e9cd6e9d5acbe528a8c1a55
seming2 <popqr3@naver.com>
2025-06-09 22:09:21 +0900

    로딩스피너

---
+++ components/Loader.js
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className='flex justify-center py-10'>
+      <div className='w-6 h-6 border-2 border-gray-300 border-t-black rounded-full animate-spin' />
+    </div>
+  );
+}
